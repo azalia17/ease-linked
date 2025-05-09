628612270a2127f377c2868b1261aa37cd9298f0
azalia17 <azaliaaps17@gmail.com>
2025-05-09 13:34:21 +0700

    feat(component): minimize search card

---
+++ EaseLinked/View/Components/SearchCardMinimize.swift
@@ -0,0 +1,46 @@
+//
+//  SearchCardMinimize.swift
+//  BSDLink
+//
+//  Created by Azalia Amanda on 09/04/25.
+//
+
+import SwiftUI
+
+struct SearchCardMinimize: View {
+    let action: () -> Void
+    let from: String
+    let to: String
+    
+    var body: some View {
+        HStack(alignment: .top) {
+            HStack{
+                Image(systemName: "chevron.backward")
+                    .resizable()
+                    .frame(width: 13, height: 22)
+                    .foregroundStyle(.black)
+                    .padding(.trailing, 12)
+                Text(from)
+                    .lineLimit(1)
+                Image(systemName: "arrow.forward")
+                Text(to)
+                    .lineLimit(1)
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(12)
+            .background(
+                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                    .fill(.white)
+                    .frame(maxWidth: .infinity)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            )
+            .shadow(color: Color.black.opacity(0.3), radius: 15, x: 0, y: 10)
+        }
+        .background()
+    }
+}
+
+#Preview {
+    SearchCardMinimize(action: {}, from: "the", to: "breeze")
+}
